15b6220a7f7ddd28af0d574e30909b0f49f5a864
Guillaume <dussartguillaume@orange.fr>
2017-11-06 21:37:44 +0100

    remove phinx

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -5,13 +5,7 @@ class HomeController extends Controller
 {
 
     public function index(){
-        $var['title'] = "Accueil";
-        $this->loadModel('Post');
-        $var['realisations'] = $this->Post->findAll('works w', [
-            'join'=>['images i'=>'w.workID=i.workID'],
-            'group'=>'w.workID',
-            'limit'=>0,5
-        ]);
-        $this->Views->render('pages', 'index',$var);
+        $title = "Accueil";
+        $this->Views->render('pages', 'index',compact('title'));
     }
 }
